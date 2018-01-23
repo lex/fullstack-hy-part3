50682be2859e9b3b736a71373d115cc67f6272fb
lex <lex@users.noreply.github.com>
2018-01-23 12:13:30 +0200

    3.2

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  res.send(
+    `<p>there are ${persons.length} person${
+      persons.length == 1 ? "" : "s"
+    } in the phone book</p><p>${new Date()}</p>`
+  );
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
